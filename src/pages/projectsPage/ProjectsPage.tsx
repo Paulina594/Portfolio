5563ea8948cc14cd526a63010abb7c11980d93b8
Paulina594 <paulina.drozdz94@gmail.com>
2022-05-22 11:37:10 +0200

    created structure of TechnologiesPage and ProjectsPage, created styles file for TechnologiesPage

--- src/pages/projectsPage/ProjectsPage.tsx
+++ src/pages/projectsPage/ProjectsPage.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { ProjectCard } from "../../components/projectCard/ProjectCard";
 
+import "./ProjectsPage.scss";
+
 export const ProjectsPage = () => {
   return (
     <>
-      <h1 className="gradient-text">My work</h1>
-      <div className="page-content projects">
-        {<ProjectCard name="Metronom" />}
-        {<ProjectCard name="Dzienniczek treningowy" />}
-        {<ProjectCard name="ToDo Lista" />}
+      <div className="title-wrapper">
+        <h1 className="gradient-text">Projects</h1>
+      </div>
+      <div className="container">
+        <div className="row projects-description"></div>
+        <div className="row projects">
+          {<ProjectCard name="Metronom" />}
+          {<ProjectCard name="Dzienniczek treningowy" />}
+          {<ProjectCard name="ToDo Lista" />}
+        </div>
       </div>
     </>
   );
--- src/pages/technologiesPage/TechnologiesPage.tsx
+++ src/pages/technologiesPage/TechnologiesPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+import "./TechnologiesPage.scss";
+
 export const TechnologiesPage = () => {
   return (
     <>
-      <h1 className="gradient-text">Technologies</h1>
-      <div className="page-content">Tech</div>
+      <div className="title-wrapper">
+        <h1 className="gradient-text">Technologies</h1>
+      </div>
+      <div className="container">
+        <div className="row tech-description">
+          Tools I am currently working/playing with:{" "}
+        </div>
+        <div className="row technologies"></div>
+      </div>
     </>
   );
 };
