e64bb06593d707fd5e2e1a1650206c0f9fb52907
Paulina594 <paulina.drozdz94@gmail.com>
2022-05-25 11:48:28 +0200

    entrance animations duration time revised

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { Header } from "./modules/header/Header";
