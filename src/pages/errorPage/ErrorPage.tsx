e7363f9ef00f4c1308db6aef30827110b5a4d25d
Paulina594 <paulina.drozdz94@gmail.com>
2022-05-26 19:37:35 +0200

    error page content implemented, error404.png added

--- src/pages/errorPage/ErrorPage.tsx
+++ src/pages/errorPage/ErrorPage.tsx
@@ -1,5 +1,25 @@
+import React from "react";
+
+import "./ErrorPage.scss";
+
+import errorImg from "../../assets/images/error404.png";
+
 export const ErrorPage = () => {
-  return(
-    <></>
-  )
-}
+  return (
+    <>
+      <div className="title-wrapper">
+        <h1 className="gradient-text slide-in-left">Error 404</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-left slide-in-fwd-center">
+            Oops! We can't seem to find the page you're looking for.
+          </div>
+          <div className="col-right fade-in">
+            <img className="error" src={errorImg} alt="laptop with error 404" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
