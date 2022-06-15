aca6ccc1634bd87eb6090253a9c29bef4efb1cad
Paulina594 <paulina.drozdz94@gmail.com>
2022-06-15 15:19:09 +0200

    modal entrance animation changed

--- src/components/projectDescriptionModal/ProjectDescriptionModal.tsx
+++ src/components/projectDescriptionModal/ProjectDescriptionModal.tsx
@@ -52,70 +52,75 @@ export const ProjectDescriptionModal = ({
     <>
       <div className="blur">
         <div className="modal-wrapper">
-          <div className="modal scale-in-center-modal" ref={modal}>
-            <button className="exit" onClick={setIsModalOpen}>
-              <i className="fa-solid fa-xmark" />
-            </button>
-            <h2 className="gradient-text">{project.name}</h2>
-            <div className="modal-row summary">
-              <div className="image">
-                <img src={project.image} alt={project.name} />
-              </div>
-              <div className="project-description">
-                <div>
-                  {project.description.text.map((p: string) => (
-                    <p>{p}</p>
-                  ))}
+          <div className="modal scale-in-hor-center" ref={modal}>
+            <div className="modal-content fade-in-modal-content">
+              <button className="exit" onClick={setIsModalOpen}>
+                <i className="fa-solid fa-xmark" />
+              </button>
+              <h2 className="gradient-text">{project.name}</h2>
+              <div className="modal-row summary">
+                <div className="image">
+                  <img src={project.image} alt={project.name} />
+                </div>
+                <div className="project-description">
+                  <div>
+                    {project.description.text.map((p: string) => (
+                      <p>{p}</p>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="modal-row project-technologies">
-              <h3>Project based on:</h3>
-              {techList}
-            </div>
-            <div className="modal-row preview">
-              <h3>Take a look at the project:</h3>
-              <a
-                href={project.description.preview}
-                target="_blank"
-                rel="noreferrer"
-                className="button2"
-              >
-                <span />
-                <span />
-                <span />
-                <span />
-                <i className="fa-regular fa-eye" /> Demo
-              </a>
-
-              <a
-                href={project.description.github}
-                target="_blank"
-                rel="noreferrer"
-                className="button2"
-              >
-                <span />
-                <span />
-                <span />
-                <span />
-                <i className="fa-brands fa-github" /> GitHub
-              </a>
-            </div>
-            <div className="modal-row screenshots">
-              <h3>Screenshots:</h3>
-              <div className="gallery">
-                {project.description.screenshots.map(
-                  (photo: string, index: number) => (
-                    <div key={index} className="screenshot">
-                      <img src={photo} alt={`Screenshot number ${index + 1}`} />
-                    </div>
-                  )
-                )}
+              <div className="modal-row project-technologies">
+                <h3>Project based on:</h3>
+                {techList}
               </div>
-            </div>
-            <div className="modal-row other-tools">
-              <h3>Other tools used:</h3>
-              <ul>{otherTools}</ul>
+              <div className="modal-row preview">
+                <h3>Take a look at the project:</h3>
+                <a
+                  href={project.description.preview}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="button2"
+                >
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                  <i className="fa-regular fa-eye" /> Demo
+                </a>
+
+                <a
+                  href={project.description.github}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="button2"
+                >
+                  <span />
+                  <span />
+                  <span />
+                  <span />
+                  <i className="fa-brands fa-github" /> GitHub
+                </a>
+              </div>
+              <div className="modal-row screenshots">
+                <h3>Screenshots:</h3>
+                <div className="gallery">
+                  {project.description.screenshots.map(
+                    (photo: string, index: number) => (
+                      <div key={index} className="screenshot">
+                        <img
+                          src={photo}
+                          alt={`Screenshot number ${index + 1}`}
+                        />
+                      </div>
+                    )
+                  )}
+                </div>
+              </div>
+              <div className="modal-row other-tools">
+                <h3>Other tools used:</h3>
+                <ul>{otherTools}</ul>
+              </div>
             </div>
           </div>
         </div>
